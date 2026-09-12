5c77f3325ce83b04ab5d00231680909d1b346902
Antoine Aflalo (Belphemur) <197810+Belphemur@users.noreply.github.com>
2026-09-12 00:48:24 +0000

    [P2b] style: format new progress manager test stub

--- test/progress_manager/stubs/Stubs.cpp
+++ test/progress_manager/stubs/Stubs.cpp
@@ -1,8 +1,8 @@
 // Definitions for the progress_manager host-test stubs.
 
-#include "freertos/FreeRTOS.h"
 #include "HalPowerManager.h"
 #include "HalStorage.h"
+#include "freertos/FreeRTOS.h"
 
 // Test-controllable wall clock (declared in the suite via extern).
 unsigned long testClockMs = 0;
