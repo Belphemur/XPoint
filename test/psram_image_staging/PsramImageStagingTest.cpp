879fbd66b0359f065bef0eac7817d66e1ad0ee19
Antoine Aflalo (Belphemur) <197810+Belphemur@users.noreply.github.com>
2026-09-13 02:28:54 +0000

    feat(epub): add guarded PSRAM image staging helper
    [task2]

---
+++ test/psram_image_staging/PsramImageStagingTest.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <cstdint>
+
+#include "Epub/ImageStaging.h"
+
+namespace {
+
+TEST(ImageStagingGuard, AcceptsImageAtTheLimit) {
+  EXPECT_TRUE(isImageSizeWithinPsramLimit(MAX_IMAGE_FILE_SIZE));
+}
+
+TEST(ImageStagingGuard, RejectsImageAboveTheLimit) {
+  EXPECT_FALSE(isImageSizeWithinPsramLimit(MAX_IMAGE_FILE_SIZE + 1));
+  EXPECT_FALSE(isImageSizeWithinPsramLimit(SIZE_MAX));
+}
+
+}  // namespace
